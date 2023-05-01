36cead75eb17f2ac13eaec9ab2a7b38dd51f636d
XnextOneO <xnextone@gmail.com>
2023-05-01 15:36:59 +0300

    feat change 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,33 +1,94 @@
 import React from 'react';
-import Image from 'next/image';
-import BgImg from '@public/assets/404bg.svg';
 import styled from 'styled-components';
 
-const Page = styled.div`
-  height: 100%;
-  width: 100%;
+const SpotlightWrap = styled.div`
+  background: url('https://static-cse.canva.com/blob/846900/photo1502082553048f009c37129b9e1583341920812.jpeg')
+    no-repeat center center;
+  background-size: cover;
+  height: 100vh;
+  position: relative;
 `;
 
-const Content = styled.div`
+const SpotlightLink = styled.a`
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-shadow: 0 0 10px whitesmoke, 0 0 20px whitesmoke, 0 0 30px whitesmoke,
+    0 0 40px whitesmoke;
+  font-size: 1.5rem;
+  text-decoration: none;
+
+  &:hover {
+    color: #fff;
+  }
+`;
+
+const Spotlight = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-image: radial-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8));
+`;
+
+const ButtonErrorLink = styled.button`
+  background-color: white;
+  border: none;
+  color: black;
+  padding: 16px 32px;
+  text-align: center;
+  font-size: 16px;
+  margin: 4px 2px;
+  opacity: 0.6;
+  transition: 0.3s;
+  display: inline-block;
+  text-decoration: none;
+  cursor: pointer;
+  border-radius: 1em;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const ContentWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
+  z-index: 1;
 `;
+// const Migs = styled.span`
+//   animation: neon-animation 0.1s ease-in-out infinite alternate;
+// `;
 
-export default function Page404() {
-  return (
-    <Page>
-      <Image src={BgImg} alt="404" fill />
-      <Content>
-        <h1>404 not found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-      </Content>
-    </Page>
-  );
+class SpotlightComponent extends React.Component {
+  onMouseMove = (event: React.MouseEvent) => {
+    const spotlight = document.querySelector('.spotlight') as HTMLElement;
+    if (spotlight && spotlight.offsetWidth) {
+      const w = spotlight.offsetWidth;
+      const h = spotlight.offsetHeight;
+      const t = event.pageY - spotlight.offsetTop;
+      const l = event.pageX - spotlight.offsetLeft;
+      spotlight.style.backgroundImage = `radial-gradient(circle at ${
+        (l / w) * 100
+      }% ${(t / h) * 100}%, transparent 80px, rgba(0, 0, 0, 0.99) 400px)`;
+    }
+  };
+
+  render() {
+    return (
+      <SpotlightWrap onMouseMove={this.onMouseMove}>
+        <ContentWrap>
+          <SpotlightLink href="#link">Page not found</SpotlightLink>
+          <ButtonErrorLink>Back to mainpage :(</ButtonErrorLink>
+        </ContentWrap>
+        <Spotlight className="spotlight"></Spotlight>
+      </SpotlightWrap>
+    );
+  }
 }
+
+export default SpotlightComponent;
